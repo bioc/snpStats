0ad6fb0589c1ccc357d0d88da6f4cb7c8e513dc3
vjcitn <stvjc@channing.harvard.edu>
2026-03-15 19:00:05 -0400

    adding backport, to devel... probably not needed

--- src/Rmissing.h
+++ src/Rmissing.h
@@ -1,4 +1,10 @@
 /* This function is declared in the R source package, in src/include/Defn.h, 
    and defined in src/main/attrib.c, but not declared in any of the "user"
    include files. There is probably a way of avoiding its use */
+
+#include <Rversion.h>
+#if R_VERSION < R_Version(4, 6, 0)
+# define R_class(x) R_data_class(x, FALSE)
+#endif
+
 SEXP R_data_class(SEXP obj, Rboolean singleString);
